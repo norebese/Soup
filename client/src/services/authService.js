8cadf3b35ee01e27b28eda4fa3c288a349209042
norebese <ehddls1029@naver.com>
2024-06-30 10:51:18 +0900

    ok

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -6,7 +6,20 @@ const API_URL = 'http://localhost:3000';
 export const loginService = async (id, password) => {
   try {
     const response = await axios.post(`${API_URL}/login`, { id, password });
-    return response.data;
+
+    if (response.status >= 200 && response.status < 300) {
+      // 성공 응답 (상태 코드 200대)
+      return response.data;
+    } else if (response.status >= 300 && response.status < 400) {
+      // 리다이렉션 응답 (상태 코드 300대)
+      console.warn('Redirection response:', response);
+    } else if (response.status >= 400 && response.status < 500) {
+      // 클라이언트 오류 응답 (상태 코드 400대)
+      console.error('Client error:', response);
+    } else if (response.status >= 500) {
+      // 서버 오류 응답 (상태 코드 500대)
+      console.error('Server error:', response);
+    }
   } catch (error) {
     console.log(`(authService.js) id: ${id} password: ${password}`)
     console.error('Login error:', error);
